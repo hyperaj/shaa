8799e5735ce96e39fdd3d0eab82e27893d74512b
hyperaj <168776874+hyperaj@users.noreply.github.com>
2024-05-29 21:31:00 +0530

    Update extras.py

--- ThavaXMusic/utils/inline/extras.py
+++ ThavaXMusic/utils/inline/extras.py
@@ -33,7 +33,7 @@ def supp_markup(_):
             [
                 InlineKeyboardButton(
                     text=_["S_B_9"],
-                    url=SUPPORT_CHAT,
+                    url=f"https://t.me/Team_Hypers_Networks,
                 ),
             ]
         ]
