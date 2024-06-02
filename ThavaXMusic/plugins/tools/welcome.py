4f46c18538129968c346a0ef0b7584603953d46e
hyperaj <168776874+hyperaj@users.noreply.github.com>
2024-06-02 11:59:17 +0530

    Update welcome.py

--- ThavaXMusic/plugins/tools/welcome.py
+++ ThavaXMusic/plugins/tools/welcome.py
@@ -121,7 +121,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
   #  draw.text((0, 0), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
 
     #
-    pfp_position = (400, 500)
+    pfp_position = (300, 500)
     background.paste(pfp, pfp_position, pfp)
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
